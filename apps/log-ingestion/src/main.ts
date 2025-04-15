0b39649387a7c368fe81259fe29d92989428f112
Amr Mubarak <devtestamr@gmail.com>
2025-04-15 10:37:17 +0200

    refactor: remove Kafka module and related files; update log ingestion module imports

--- apps/log-ingestion/src/main.ts
+++ apps/log-ingestion/src/main.ts
@@ -1,10 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import {
-  GrpcOptions,
-  MicroserviceOptions,
-  Transport,
-} from '@nestjs/microservices';
+import { AppModule } from './logs/log.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { LOGS } from '@app/proto/index';
 
